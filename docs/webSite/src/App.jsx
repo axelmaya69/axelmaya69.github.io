47781d828677c04a7a6a334fb2edbd0a1c1e5f4a
axelmaya69 <axeldbmaya96@gmail.com>
2026-02-17 22:38:04 -0600

    importando tailwind a clase

--- docs/webSite/src/App.jsx
+++ docs/webSite/src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='bg-red-700 flex align-middle justify-center flex-col h-screen w-screen'> 
+    <h1 className='text-maroon-700 text-4xl font-bold border-4 border-yellow-300 flex justify-center sm:p-4 sm:m-4'>Mi CV</h1>
+    <h2 className='text-yellow-300 text-2xl font-semibold border-4 border-yellow-300 flex justify-center sm:p-2 sm:m-2 sm:mx-4'>Ing. Axel Daniel Bartolo Maya - Ingeniero En Sistemas Computacionales</h2>
+    
+    <p className='text-white p-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut doloribus nihil error autem. Aut quis magnam debitis, mollitia ut repellat cumque dolorem nam sint amet aliquid quia, magni tempora ipsam!</p>
+    </div>
     </>
   )
 }
